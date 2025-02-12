b2f090898a9a46bc3f353f502b52de70b5fbe40f
jonathanduran7 <joniduran624@gmail.com>
2025-02-11 21:45:26 -0300

    feat: implement currency module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CurrencyModule],
   controllers: [AppController],
   providers: [AppService],
 })
