5c2ad1702db12ea475c5fd5735bea40df854c93e
jonathanduran7 <joniduran624@gmail.com>
2025-02-11 21:10:08 -0300

    feat: remove config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('PG_DATABASE_HOST'),
-        port: configService.get('PG_DATABASE_PORT'),
-        username: configService.get('PG_DATABASE_USER'),
-        password: configService.get('PG_DATABASE_PASSWORD'),
-        database: configService.get('PG_DATABASE_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
-  ],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
