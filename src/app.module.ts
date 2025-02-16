c3c19dc65d6c6847a22338ad70e28044771ab85c
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 13:17:09 -0300

    feat: add account module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CurrencyModule } from './currency/currency.module';
 import { CategoryModule } from './category/category.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [DatabaseModule, CurrencyModule, CategoryModule],
+  imports: [DatabaseModule, CurrencyModule, CategoryModule, AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
