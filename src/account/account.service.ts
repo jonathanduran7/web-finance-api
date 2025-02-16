4501a0d7ba6a72b583ba7f553dc1c691e48a38ef
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 13:29:29 -0300

    feat(account): implement create function in service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { BaseService } from 'src/core/base.service';
+import { Account } from './account.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AccountService {}
+export class AccountService extends BaseService<Account> {
+  constructor(
+    @InjectRepository(Account)
+    repository: Repository<Account>,
+  ) {
+    super(repository);
+  }
+
+  async create(data: any): Promise<void> {
+    const findAccount = await this.repository.findOne({
+      where: { name: data?.name },
+    });
+    if (findAccount) {
+      throw new BadRequestException('Account already exists');
+    }
+    await this.repository.save(data);
+  }
+}
