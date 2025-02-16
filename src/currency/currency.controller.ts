a04eb33a5ae5b81a937387c301f7ae9444916623
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 11:15:22 -0300

    feat(currency): implement dto in controller

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -2,10 +2,11 @@ import { Controller } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { CurrencyService } from './currency.service';
 import { Currency } from './currency.entity';
+import { CurrencyDto } from './currency.dto';
 
 @Controller('currency')
-export class CurrencyController extends BaseController<Currency> {
+export class CurrencyController extends BaseController<Currency, CurrencyDto> {
   constructor(protected readonly CurrencyService: CurrencyService) {
-    super(CurrencyService);
+    super(CurrencyService, CurrencyDto);
   }
 }
