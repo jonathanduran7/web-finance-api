5421fc85a7643b2f89d602cd85ce2ab083e23cc2
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 12:21:43 -0300

    test(currency): test crud method

--- src/currency/currency.controller.spec.ts
+++ src/currency/currency.controller.spec.ts
@@ -45,4 +45,39 @@ describe('CurrencyController', () => {
     const result = await controller.getBase();
     expect(result).toEqual([{ id: 1, name: 'USD' }]);
   });
+
+  it('should return currency ', async () => {
+    serviceMock.findById.mockReturnValue({ id: 1, name: 'USD' });
+    const result = await controller.getBaseById('1');
+    expect(result).toEqual({ id: 1, name: 'USD' });
+  });
+
+  it('should create currency ', async () => {
+    const data = { id: 1, name: 'USD' };
+    serviceMock.create.mockReturnValue(data);
+    const result = await controller.postBase(data);
+    expect(result).toEqual(data);
+  });
+
+  it('should update currency ', async () => {
+    const data = { id: 1, name: 'USD' };
+    serviceMock.update.mockReturnValue(data);
+    const result = await controller.putBase('1', data);
+    expect(result).toEqual(data);
+  });
+
+  it('should delete currency ', async () => {
+    serviceMock.delete.mockReturnValue(undefined);
+    const result = await controller.deleteBase(1);
+    expect(result).toBeUndefined();
+  });
+
+  it('should show error when currency not found ', async () => {
+    serviceMock.findById.mockReturnValue(null);
+    try {
+      await controller.getBaseById('1');
+    } catch (e) {
+      expect(e.message).toBe('Data not found');
+    }
+  });
 });
