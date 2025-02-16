66fc1ec9f252f2e1df2c9aa463eab54539c50077
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 10:01:48 -0300

    feat(currency): modify controller

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,9 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { CurrencyService } from './currency.service';
+import { Currency } from './currency.entity';
 
 @Controller('currency')
-export class CurrencyController extends BaseController {
+export class CurrencyController extends BaseController<Currency> {
   constructor(protected readonly CurrencyService: CurrencyService) {
     super(CurrencyService);
   }
