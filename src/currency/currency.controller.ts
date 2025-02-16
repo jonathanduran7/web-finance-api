eb4864e9b8ba03acb09c410bebac292baa21f538
jonathanduran7 <joniduran624@gmail.com>
2025-02-15 23:24:44 -0300

    feat(currency): implement service in controller

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,5 +1,10 @@
 import { Controller } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
+import { CurrencyService } from './currency.service';
 
 @Controller('currency')
-export class CurrencyController extends BaseController {}
+export class CurrencyController extends BaseController {
+  constructor(protected readonly CurrencyService: CurrencyService) {
+    super(CurrencyService);
+  }
+}
