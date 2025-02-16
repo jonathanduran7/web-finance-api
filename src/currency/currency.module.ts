d0abd1019af8a142f0068e0ad4a9df1ffceab9a4
jonathanduran7 <joniduran624@gmail.com>
2025-02-15 23:04:24 -0300

    feat(currency): implement controller in module

--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { Currency } from './currency.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CurrencyController } from './currency.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Currency])],
   exports: [],
   providers: [],
+  controllers: [CurrencyController],
 })
 export class CurrencyModule {}
