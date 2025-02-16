d26bcef1fac2347208bd394e31b11735117f3dcf
jonathanduran7 <joniduran624@gmail.com>
2025-02-15 23:04:06 -0300

    feat(currency): add controller

---
+++ src/currency/currency.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common';
+import { BaseController } from 'src/core/base.controller';
+
+@Controller('currency')
+export class CurrencyController extends BaseController {}
