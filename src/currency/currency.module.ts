4a5d0f2d3ceccd3b122791781fdca9bb59982b4f
jonathanduran7 <joniduran624@gmail.com>
2025-02-15 23:05:58 -0300

    feat(currency): add service

--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { Currency } from './currency.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CurrencyController } from './currency.controller';
+import { CurrencyService } from './currency.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Currency])],
   exports: [],
-  providers: [],
+  providers: [CurrencyService],
   controllers: [CurrencyController],
 })
 export class CurrencyModule {}
