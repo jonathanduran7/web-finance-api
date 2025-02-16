e75f8ef957ec9f7ffc7ae19e7ec69393b48bbe50
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 13:47:42 -0300

    feat(account): implement currency module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { Account } from './account.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountService } from './account.service';
+import { AccountController } from './account.controller';
+import { CurrencyModule } from 'src/currency/currency.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), CurrencyModule],
   providers: [AccountService],
+  controllers: [AccountController],
 })
 export class AccountModule {}
