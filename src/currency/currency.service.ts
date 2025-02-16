146ab61b7ff690dcb1f64e7030342b26a7518650
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 10:01:24 -0300

    feat(currency): modify service with repository

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,10 +1,15 @@
-import { Get, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
+import { Currency } from './currency.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CurrencyService extends BaseService {
-  @Get()
-  findAll(): { data: string } {
-    return { data: 'findAll desde currency' };
+export class CurrencyService extends BaseService<Currency> {
+  constructor(
+    @InjectRepository(Currency)
+    repository: Repository<Currency>,
+  ) {
+    super(repository);
   }
 }
