ee52dfddb8128b61ff7050bb8a55af622685a891
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 12:29:42 -0300

    feat(category): add service

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
+import { CategoryService } from './category.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
+  providers: [CategoryService],
 })
 export class CategoryModule {}
