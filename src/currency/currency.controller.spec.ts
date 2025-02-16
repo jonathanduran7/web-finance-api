e45ccb1be8885eb502e65df77d8ddb5d02a2806e
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 12:19:58 -0300

    test(currency): initial test controller

--- src/currency/currency.controller.spec.ts
+++ src/currency/currency.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrencyController } from './currency.controller';
+import { CurrencyService } from './currency.service';
 
 describe('CurrencyController', () => {
   let controller: CurrencyController;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let service: CurrencyService;
+
+  const serviceMock = {
+    findAll: jest.fn(),
+    findById: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CurrencyController],
+      providers: [
+        {
+          provide: CurrencyService,
+          useValue: serviceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<CurrencyController>(CurrencyController);
+    service = module.get<CurrencyService>(CurrencyService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return empty currencies ', async () => {
+    serviceMock.findAll.mockReturnValue([]);
+    const result = await controller.getBase();
+    expect(result).toEqual([]);
   });
 });
