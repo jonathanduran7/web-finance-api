6915386a4e65e1bbf288663db2a2384fe4917d68
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 13:24:51 -0300

    feat(account): add service

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Account } from './account.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountService } from './account.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Account])],
+  providers: [AccountService],
 })
 export class AccountModule {}
