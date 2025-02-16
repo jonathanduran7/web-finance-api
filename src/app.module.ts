30be477e3c195720943947e18b23fb1166c112af
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 12:26:05 -0300

    feat: add category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CurrencyModule } from './currency/currency.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [DatabaseModule, CurrencyModule],
+  imports: [DatabaseModule, CurrencyModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
