0ad39017654f1e621f605a3a598e15abad4076ec
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 13:47:05 -0300

    feat(account): implement currency service for create account

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -3,12 +3,14 @@ import { BaseService } from 'src/core/base.service';
 import { Account } from './account.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CurrencyService } from 'src/currency/currency.service';
 
 @Injectable()
 export class AccountService extends BaseService<Account> {
   constructor(
     @InjectRepository(Account)
     repository: Repository<Account>,
+    private currencyService: CurrencyService,
   ) {
     super(repository);
   }
@@ -20,6 +22,16 @@ export class AccountService extends BaseService<Account> {
     if (findAccount) {
       throw new BadRequestException('Account already exists');
     }
+
+    const findCurrency = await this.currencyService.findById(data?.currencyId);
+
+    if (!findCurrency) {
+      throw new BadRequestException('Currency not found');
+    }
+
+    data.initialBalance = data?.initialBalance || 0;
+    data.currency = findCurrency;
+
     await this.repository.save(data);
   }
 }
