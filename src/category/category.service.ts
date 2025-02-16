7d3472e17ea5888f2f9879bfaa15a84edc60ed05
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 12:31:19 -0300

    feat(category): extends service from base service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { BaseService } from 'src/core/base.service';
+import { Category } from './category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService extends BaseService<Category> {
+  constructor(
+    @InjectRepository(Category)
+    repository: Repository<Category>,
+  ) {
+    super(repository);
+  }
+}
