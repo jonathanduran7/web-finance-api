dae4e569792a82bfcc42d2036aa1cf7c85e0073a
jonathanduran7 <joniduran624@gmail.com>
2025-02-16 14:40:28 -0300

    test(account): initial test

--- src/account/account.service.spec.ts
+++ src/account/account.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountService } from './account.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Account } from './account.entity';
+import { CurrencyService } from 'src/currency/currency.service';
 
 describe('AccountService', () => {
   let service: AccountService;
 
+  const serviceMock = {
+    findAll: jest.fn(),
+    findById: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  const serviceCurrencyMock = {
+    findById: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountService],
+      providers: [
+        AccountService,
+        {
+          provide: getRepositoryToken(Account),
+          useValue: serviceMock,
+        },
+        {
+          provide: CurrencyService,
+          useValue: serviceCurrencyMock,
+        },
+      ],
     }).compile();
 
     service = module.get<AccountService>(AccountService);
