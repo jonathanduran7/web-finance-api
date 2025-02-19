2ff32ed4bf1485ec40d74269204bd07d108ea078
jonathanduran7 <joniduran624@gmail.com>
2025-02-19 15:34:58 -0300

    feat(transaction): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { DatabaseModule } from './database/database.module';
 import { CurrencyModule } from './currency/currency.module';
 import { CategoryModule } from './category/category.module';
 import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [DatabaseModule, CurrencyModule, CategoryModule, AccountModule],
+  imports: [
+    DatabaseModule,
+    CurrencyModule,
+    CategoryModule,
+    AccountModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
