7dad7e9d17be33cebbc7de39b1f6035ca58bb42d
jonathanduran7 <joniduran624@gmail.com>
2025-02-20 22:54:43 -0300

    test(transaction: testing find all method

--- src/transaction/transaction.service.spec.ts
+++ src/transaction/transaction.service.spec.ts
@@ -1,12 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionService } from './transaction.service';
+import { Transaction } from './transaction.entity';
+import { AccountService } from 'src/account/account.service';
+import { CategoryService } from 'src/category/category.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('TransactionService', () => {
   let service: TransactionService;
 
+  const serviceAccountMock = {
+    findById: jest.fn(),
+  };
+
+  const serviceCategoryMock = {
+    findById: jest.fn(),
+  };
+
+  const serviceMock = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService],
+      providers: [
+        TransactionService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useValue: serviceMock,
+        },
+        {
+          provide: AccountService,
+          useValue: serviceAccountMock,
+        },
+        {
+          provide: CategoryService,
+          useValue: serviceCategoryMock,
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionService>(TransactionService);
@@ -14,5 +48,11 @@ describe('TransactionService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return empty transactions', async () => {
+    serviceMock.find.mockReturnValue([]);
+    const result = await service.findAll();
+    expect(result).toEqual([]);
   });
 });
