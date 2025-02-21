0e4ddd4c941dc52b22ea34ccc9e56140034fc9ad
jonathanduran7 <joniduran624@gmail.com>
2025-02-20 21:51:48 -0300

    feat(transaction): import modules account and category

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './transaction.entity';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { AccountModule } from 'src/account/account.module';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction]),
+    AccountModule,
+    CategoryModule,
+  ],
   providers: [TransactionService],
   controllers: [TransactionController],
 })
