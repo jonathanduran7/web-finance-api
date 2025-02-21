67e5ce324f2bbb9f05afd88cb30699b6429987c2
jonathanduran7 <joniduran624@gmail.com>
2025-02-20 21:50:30 -0300

    feat(transaction): modify create method in service

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
 import { Transaction } from './transaction.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CategoryService } from 'src/category/category.service';
+import { AccountService } from 'src/account/account.service';
 
 @Injectable()
 export class TransactionService extends BaseService<Transaction> {
   constructor(
     @InjectRepository(Transaction)
     repository: Repository<Transaction>,
+    private categoryService: CategoryService,
+    private accountService: AccountService,
   ) {
     super(repository);
+  }
+
+  async create(data: any): Promise<void> {
+    const findAccount = await this.accountService.findById(data.accountId);
+
+    if (!findAccount) {
+      throw new BadRequestException('Account not found');
+    }
+
+    const findCategory = await this.categoryService.findById(data.categoryId);
+
+    if (!findCategory) {
+      throw new BadRequestException('Category not found');
+    }
+
+    data.account = findAccount;
+    data.category = findCategory;
+
+    await this.repository.save(data);
   }
 }
