db8c8f08bcaaef63e965cece05cbd11465f2df67
jonathanduran7 <joniduran624@gmail.com>
2025-02-20 21:25:33 -0300

    feat(transaction): add module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './transaction.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Transaction])],
+})
 export class TransactionModule {}
