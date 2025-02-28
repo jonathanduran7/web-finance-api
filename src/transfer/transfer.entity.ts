6a37db6b2c23fc929b88bb8a03d93f939d2569ea
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:09:19 -0300

    feat(transfer): create Transfer entity with relationships to Account

---
+++ src/transfer/transfer.entity.ts
@@ -0,0 +1,40 @@
+import { Account } from 'src/account/account.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('transfers')
+export class Transfer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+  amount: number;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+
+  @ManyToOne(() => Account, (account) => account.sourceTransfers, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'sourceAccountId' })
+  sourceAccount: Account;
+
+  @ManyToOne(() => Account, (account) => account.destinationTransfers, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'destinationAccountId' })
+  destinationAccount: Account;
+}
