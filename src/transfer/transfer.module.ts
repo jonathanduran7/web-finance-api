8f027d94f820bb9a6c5f31fb6b2caf12fdd15ba9
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:29:34 -0300

    feat(transfer): import AccountModule in TransferModule

--- src/transfer/transfer.module.ts
+++ src/transfer/transfer.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transfer } from './transfer.entity';
 import { TransferService } from './transfer.service';
 import { TransferController } from './transfer.controller';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transfer])],
+  imports: [TypeOrmModule.forFeature([Transfer]), AccountModule],
   controllers: [TransferController],
   providers: [TransferService],
 })
