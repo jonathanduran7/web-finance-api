13670aa5cd515fbde03164e7098fa268505989a7
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:15:09 -0300

    feat(transfer): add TransferDTO for transfer data validation

---
+++ src/transfer/transfer.dto.ts
@@ -0,0 +1,23 @@
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class TransferDTO {
+  @IsNumber({
+    allowNaN: false,
+    allowInfinity: false,
+    maxDecimalPlaces: 2,
+  })
+  amount: number;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsNumber()
+  sourceAccountId: number;
+
+  @IsNumber()
+  destinationAccountId: number;
+
+  @IsDateString()
+  date: Date;
+}
