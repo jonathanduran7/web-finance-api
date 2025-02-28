de96584e75f5f4caf8196942e0c0c9962af9b343
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:35:11 -0300

    feat(transfer): add update method to TransferService with validation for accounts and transfer existence

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
 import { Transfer } from './transfer.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { AccountService } from 'src/account/account.service';
 
 @Injectable()
@@ -44,5 +44,41 @@ export class TransferService extends BaseService<Transfer> {
     });
 
     await this.repository.save(transfer);
+  }
+
+  async update(id: any, data: any): Promise<UpdateResult> {
+    const [sourceAccount, destinationAccount, transfer] = await Promise.all([
+      this.accountService.findById(data.sourceAccountId),
+      this.accountService.findById(data.destinationAccountId),
+      this.repository.findOne({ where: { id } }),
+    ]);
+
+    if (!transfer) {
+      throw new BadRequestException('Transfer not found');
+    }
+
+    if (!sourceAccount) {
+      throw new BadRequestException('Source account not found');
+    }
+
+    if (!destinationAccount) {
+      throw new BadRequestException('Destination account not found');
+    }
+
+    if (sourceAccount.id === destinationAccount.id) {
+      throw new BadRequestException(
+        'Source and destination accounts must be different',
+      );
+    }
+
+    delete data.sourceAccountId;
+    delete data.destinationAccountId;
+
+    return this.repository.update(id, {
+      ...data,
+      sourceAccount,
+      destinationAccount,
+      updatedAt: new Date(),
+    });
   }
 }
