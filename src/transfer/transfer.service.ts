85d5caae98d27e02851ba8d0780048cbe48abd89
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:29:46 -0300

    feat(transfer): enhance create method to validate accounts and handle transfers

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
 import { Transfer } from './transfer.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AccountService } from 'src/account/account.service';
 
 @Injectable()
 export class TransferService extends BaseService<Transfer> {
   constructor(
     @InjectRepository(Transfer)
     repository: Repository<Transfer>,
+    private readonly accountService: AccountService,
   ) {
     super(repository);
+  }
+
+  async create(data: any): Promise<void> {
+    const [sourceAccount, destinationAccount] = await Promise.all([
+      this.accountService.findById(data.sourceAccountId),
+      this.accountService.findById(data.destinationAccountId),
+    ]);
+
+    if (!sourceAccount) {
+      throw new BadRequestException('Source account not found');
+    }
+
+    if (!destinationAccount) {
+      throw new BadRequestException('Destination account not found');
+    }
+
+    if (sourceAccount.id === destinationAccount.id) {
+      throw new BadRequestException(
+        'Source and destination accounts must be different',
+      );
+    }
+
+    const transfer = this.repository.create({
+      ...data,
+      sourceAccount,
+      destinationAccount,
+      createdAt: data.date ?? new Date(),
+      updatedAt: data.date ?? new Date(),
+    });
+
+    await this.repository.save(transfer);
   }
 }
