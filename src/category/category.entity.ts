47fc0ded98e2cbad61b0eb733d07738532e3055a
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 15:39:36 -0300

    feat(category): add one-to-many relationship with Transaction entity

--- src/category/category.entity.ts
+++ src/category/category.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Transaction } from 'src/transaction/transaction.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('categories')
 export class Category {
@@ -7,4 +8,7 @@ export class Category {
 
   @Column()
   name: string;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.category)
+  transactions: Transaction[];
 }
