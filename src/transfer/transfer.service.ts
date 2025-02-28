4bb5ba09bb34a4b01fb728be27a8a9299cf0ad6e
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:21:01 -0300

    feat(transfer): inject Transfer repository into TransferService constructor

--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from 'src/core/base.service';
 import { Transfer } from './transfer.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TransferService extends BaseService<Transfer> {}
+export class TransferService extends BaseService<Transfer> {
+  constructor(
+    @InjectRepository(Transfer)
+    repository: Repository<Transfer>,
+  ) {
+    super(repository);
+  }
+}
