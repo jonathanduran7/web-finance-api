d348fc5968bd246f14e213f2d60eb1207fda88b1
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:09:40 -0300

    feat(transfer): configure TransferModule with TypeOrm for Transfer entity

--- src/transfer/transfer.module.ts
+++ src/transfer/transfer.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transfer } from './transfer.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Transfer])],
+  controllers: [],
+  providers: [],
+})
 export class TransferModule {}
