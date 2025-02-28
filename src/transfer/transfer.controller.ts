550595511caf172e18974ac1fa5c0a26612ed398
jonathanduran7 <joniduran624@gmail.com>
2025-02-28 11:41:13 -0300

    feat(transfer): add query endpoint to TransferController for paginated and filtered transfer retrieval

--- src/transfer/transfer.controller.ts
+++ src/transfer/transfer.controller.ts
@@ -1,12 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseEnumPipe, Query } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { Transfer } from './transfer.entity';
 import { TransferDTO } from './transfer.dto';
 import { TransferService } from './transfer.service';
 
+enum Order {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
 @Controller('transfer')
 export class TransferController extends BaseController<Transfer, TransferDTO> {
   constructor(protected readonly transferService: TransferService) {
     super(transferService, TransferDTO);
+  }
+
+  @Get('query')
+  query(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Query('order', new ParseEnumPipe(Order)) order?: Order,
+    @Query('search') search?: string,
+    @Query('filters') filters?: Record<string, string>,
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
+  ) {
+    return this.transferService.query(
+      page,
+      limit,
+      order,
+      search,
+      filters,
+      startDate,
+      endDate,
+    );
   }
 }
