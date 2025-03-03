7fef5c3a23f2e8c6523f967ae11d082759929d28
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 13:39:59 -0300

    feat(category): add getBase method to retrieve categories for the current user

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,12 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { Category } from './category.entity';
 import { CategoryDto } from './category.dto';
 import { CategoryService } from './category.service';
+import { GetCurrentUserId } from 'src/commons/decorators/get-current-user-id.decorator';
 
 @Controller('category')
 export class CategoryController extends BaseController<Category, CategoryDto> {
   constructor(protected readonly categoryService: CategoryService) {
     super(categoryService, CategoryDto);
+  }
+
+  @Get()
+  async getBase(@GetCurrentUserId() userId: number): Promise<Category[]> {
+    return this.categoryService.findAll({ user: { id: userId } });
   }
 }
