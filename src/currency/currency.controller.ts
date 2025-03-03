5b4dfe54582a24ef2190f086b1acbf228f56b2c7
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 13:53:08 -0300

    feat(currency): add getBase method to retrieve currencies for the current user

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,12 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { CurrencyService } from './currency.service';
 import { Currency } from './currency.entity';
 import { CurrencyDto } from './currency.dto';
+import { GetCurrentUserId } from 'src/commons/decorators/get-current-user-id.decorator';
 
 @Controller('currency')
 export class CurrencyController extends BaseController<Currency, CurrencyDto> {
   constructor(protected readonly CurrencyService: CurrencyService) {
     super(CurrencyService, CurrencyDto);
+  }
+
+  @Get()
+  async getBase(@GetCurrentUserId() userId: number): Promise<Currency[]> {
+    return this.CurrencyService.findAll({ user: { id: userId } });
   }
 }
