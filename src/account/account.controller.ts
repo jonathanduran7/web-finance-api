fcc7516cd8e21cd1d235768d401cac59e9f0892c
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 13:23:01 -0300

    feat(controller): add getBase method to retrieve accounts for the current user

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,12 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BaseController } from 'src/core/base.controller';
 import { Account } from './account.entity';
 import { AccountDto } from './account.dto';
 import { AccountService } from './account.service';
+import { GetCurrentUserId } from 'src/commons/decorators/get-current-user-id.decorator';
 
 @Controller('account')
 export class AccountController extends BaseController<Account, AccountDto> {
   constructor(protected readonly accountService: AccountService) {
     super(accountService, AccountDto);
+  }
+
+  @Get()
+  async getBase(@GetCurrentUserId() userId: number): Promise<Account[]> {
+    return this.accountService.findAll({ user: { id: userId } });
   }
 }
