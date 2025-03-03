6e570ff6d62373f5b638fdb26cf696be99b6ebad
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 13:52:46 -0300

    feat(currency): add user relationship to Currency entity

--- src/currency/currency.entity.ts
+++ src/currency/currency.entity.ts
@@ -1,5 +1,13 @@
 import { Account } from 'src/account/account.entity';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('currencies')
 export class Currency {
@@ -11,4 +19,8 @@ export class Currency {
 
   @OneToMany(() => Account, (account) => account.currency)
   accounts: Account[];
+
+  @ManyToOne(() => User)
+  @JoinColumn()
+  user: User;
 }
