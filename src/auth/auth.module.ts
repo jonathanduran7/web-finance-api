d360dfee4d5c47141d6d77749be62584aaf2def9
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 11:01:03 -0300

    feat(auth): configure JWT module with environment variables for secret and expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
