bf90ec3cd24a06c6353b355ed90ea47706f7ed92
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 11:22:04 -0300

    feat(auth): add @Public() decorator to login and register endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './auth.dto';
 import { AuthService } from './auth.service';
+import { Public } from 'src/commons/decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
+  @Public()
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
