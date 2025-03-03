d417487840928e05e6dc0187c72cd94699d66607
jonathanduran7 <joniduran624@gmail.com>
2025-03-03 10:17:46 -0300

    feat(auth): implement login functionality with LoginDto validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LoginDto } from './auth.dto';
 
 @Controller('auth')
 export class AuthController {
   @Post('login')
-  async login() {
-    return 'login';
+  async login(@Body() loginDto: LoginDto) {
+    return loginDto;
   }
 
   @Post('register')
