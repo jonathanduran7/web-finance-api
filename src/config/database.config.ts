e85471428fe6e4e34be48251b7f55e25e586bc2c
jonathanduran7 <joniduran624@gmail.com>
2025-03-04 10:53:20 -0300

    feat(database): add SSL configuration options for database connection

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -8,5 +8,7 @@ export default () => ({
     database: process.env.PG_DATABASE_NAME,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
+    ssl: process.env.PG_DATABASE_SSL,
+    sslCaPath: process.env.PG_DATABASE_SSL_CA_PATH,
   },
 });
